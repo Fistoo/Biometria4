bdd93329234ce65928395c1450de0efd753a1e54
Kacper <66553710+kacperchrost@users.noreply.github.com>
2022-03-30 12:19:25 +0200

    xd

--- src/main/java/com/example/bio4/HelloController.java
+++ src/main/java/com/example/bio4/HelloController.java
@@ -5,8 +5,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
@@ -14,6 +20,8 @@ public class HelloController {
     private int width;
     private int height;
     private File selectedFile;
+    final int PIX_SIZE = 10;
+    private BufferedImage image;
 
     @FXML
     private Label test;
@@ -38,28 +46,59 @@ public class HelloController {
         final FileChooser fc = new FileChooser();
         fc.setInitialDirectory(new File(System.getProperty("user.home")));
         selectedFile = fc.showOpenDialog(imgOutput.getScene().getWindow());
-        Image img = new Image(selectedFile.getPath());
-        imgOutput.setImage(img);
+        image = ImageIO.read(selectedFile);
+        imgOutput.setImage(convertToFxImage(image));
         imgOutput.setFitWidth(450);
         imgOutput.setFitHeight(400);
-        width = (int)img.getWidth();
-        height = (int)img.getHeight();
-        Mediana(15,-1.5);
-        Pikselizacja(15, -1.5);
-        Kuwahara(15,-1.5);
+        width = (int)image.getWidth();
+        height = (int)image.getHeight();
     }
 
     @FXML
     void initialize() {
 
     }
+    @FXML
     public void Mediana (int window, double k1) throws IOException {
 
     }
-    public void Pikselizacja (int window, double k1) throws IOException {
+    @FXML
+    protected void Pikselizacja (int window, double k1) throws IOException {
+        Raster src = image.getData();
+        WritableRaster dest = src.createCompatibleWritableRaster();
+        for(int y = 0; y < src.getHeight(); y += PIX_SIZE) {
+            for(int x = 0; x < src.getWidth(); x += PIX_SIZE) {
 
+                double[] pixel = new double[3];
+                pixel = src.getPixel(x, y, pixel);
+
+                for(int yd = y; (yd < y + PIX_SIZE) && (yd < dest.getHeight()); yd++) {
+                    for(int xd = x; (xd < x + PIX_SIZE) && (xd < dest.getWidth()); xd++) {
+                        dest.setPixel(xd, yd, pixel);
+                    }
+                }
+            }
+        }
+        BufferedImage temp = ImageIO.read(selectedFile);
+        temp.setData(dest);
+        imgOutput.setImage(convertToFxImage(temp));
     }
+    @FXML
     public void Kuwahara (int window, double k1) throws IOException {
 
+    }
+    private static Image convertToFxImage(BufferedImage image) {
+        WritableImage wr = null;
+        if (image != null) {
+            wr = new WritableImage(image.getWidth(), image.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    pw.setArgb(x, y, image.getRGB(x, y));
+                }
+            }
+        }
+
+        return new ImageView(wr).getImage();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.bio4 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.bio4 to javafx.fxml;
